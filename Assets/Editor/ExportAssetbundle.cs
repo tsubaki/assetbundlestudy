01bb13f1414f2dfc76e87f8169044d0423955902
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2015-12-11 06:20:02 -0800

    first commit

---
+++ Assets/Editor/ExportAssetbundle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class ExportAssetbundle  {
+
+	[MenuItem("Export/Assetbundle")]
+	static void Export()
+	{
+		Directory.CreateDirectory(Application.streamingAssetsPath);
+		BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.DeterministicAssetBundle);
+	}
+}
---
+++ Assets/LoadCompleted.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCompleted : MonoBehaviour {
+
+	public void Switch()
+	{
+		gameObject.SetActive(false);
+		gameObject.SetActive(true);
+	}
+}
